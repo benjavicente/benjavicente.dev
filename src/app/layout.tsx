df52e07d63b8f2f7924280365ccf51f9bd9f6fc6
Benjamín Vicente <benjavicente@uc.cl>
2024-01-25 11:38:06 -0300

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "BenjaVicente",
 	metadataBase: new URL("https://benjavicente.dev"),
+	icons: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'>🧑‍🦰</text></svg>",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
