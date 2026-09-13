8839f2942920ab004eacfa4edba5515f9322f928
Benjamín Vicente <projects@benjavicente.dev>
2026-09-13 17:49:26 -0300

    fix build

--- src/app/blog/[postSlug]/mdx/codeHiglighter.ts
+++ src/app/blog/[postSlug]/mdx/codeHiglighter.ts
@@ -4,47 +4,46 @@ import { RehypeShikiOptions } from "@shikijs/rehype";
 import slackDark from "shiki/themes/slack-dark.mjs";
 
 import {
-	transformerNotationDiff,
-	transformerNotationHighlight,
-	transformerNotationFocus,
-	transformerNotationErrorLevel,
-	transformerMetaHighlight,
-	transformerNotationWordHighlight,
+  transformerNotationDiff,
+  transformerNotationHighlight,
+  transformerNotationFocus,
+  transformerNotationErrorLevel,
+  transformerMetaHighlight,
+  transformerNotationWordHighlight,
 } from "@shikijs/transformers";
 
-const theme = JSON.parse(JSON.stringify(slackDark).replaceAll(/#222222/g, "#021217"));
-
-// The plugin otherwise loads every bundled grammar. That is most of the request time.
-const langs = [
-	"bash",
-	"css",
-	"diff",
-	"html",
-	"http",
-	"js",
-	"json",
-	"jsx",
-	"make",
-	"markdown",
-	"py",
-	"rb",
-	"shell",
-	"sql",
-	"ts",
-	"tsx",
-	"xml",
-	"yaml",
-];
+const theme = JSON.parse(
+  JSON.stringify(slackDark).replaceAll(/#222222/g, "#021217"),
+);
 
 export const rehypeShikiOptions: RehypeShikiOptions = {
-	theme,
-	langs,
-	transformers: [
-		transformerNotationDiff(),
-		transformerNotationHighlight(),
-		transformerNotationFocus(),
-		transformerNotationErrorLevel(),
-		transformerMetaHighlight(),
-		transformerNotationWordHighlight(),
-	],
+  theme,
+  langs: [
+    "bash",
+    "css",
+    "diff",
+    "html",
+    "http",
+    "js",
+    "json",
+    "jsx",
+    "make",
+    "markdown",
+    "py",
+    "rb",
+    "shell",
+    "sql",
+    "ts",
+    "tsx",
+    "xml",
+    "yaml",
+  ],
+  transformers: [
+    transformerNotationDiff(),
+    transformerNotationHighlight(),
+    transformerNotationFocus(),
+    transformerNotationErrorLevel(),
+    transformerMetaHighlight(),
+    transformerNotationWordHighlight(),
+  ],
 };
